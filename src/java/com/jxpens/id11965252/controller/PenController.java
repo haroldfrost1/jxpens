f13fc0cece272d032b46d13e34cccca2e7f5b67d
harold <haroldfrost1@hotmail.com>
2016-09-01 21:01:46 +1000

    Created fake CRUD methods in PenController and linked to creat pen page

--- src/java/com/jxpens/id11965252/controller/PenController.java
+++ src/java/com/jxpens/id11965252/controller/PenController.java
@@ -14,6 +14,34 @@ import javax.inject.Named;
 @SessionScoped
 public class PenController implements Serializable{
 
+    private Pen pen = new Pen();
+    
+    /**
+     * @return the current pen object
+     */
+    public Pen getPen(){
+        return pen;
+    }
+    
+    public String saveNewPen(){
+        // TODO: link to database
+        return "penlist";
+    }
+    
+    public String updatePen(){
+        // TODO: link to database
+        return "penlist";
+    }
+    
+    public String deletePen(int index){
+        // TODO: link to database
+        return "penlist";
+    }
+    
+    public void loadPen(int index){
+        // TODO: link to databse
+    }
+    
     /**
      * @return an ArrayList contains Pen objects
      */
